e3ca0b7aaabffac612032236cde0997e77df30f4
ahmethakanbesel <ahmethakan.besel@gmail.com>
2024-03-10 22:07:20 +0300

    restaurant-service/feat: add openapi tags

--- restaurantservice/src/main/java/com/n11/restaurantservice/controller/RestaurantController.java
+++ restaurantservice/src/main/java/com/n11/restaurantservice/controller/RestaurantController.java
@@ -9,6 +9,9 @@ import com.n11.restaurantservice.repository.RestaurantRepository;
 import com.n11.restaurantservice.request.CreateRestaurantRequest;
 import com.n11.restaurantservice.request.UpdateRestaurantRequest;
 import com.n11.restaurantservice.service.entityservice.RestaurantEntityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.geo.Point;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +26,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/restaurants")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Restaurants", description = "The Restaurant API")
+
 public class RestaurantController {
     private final RestaurantControllerContract restaurantControllerContract;
 
     @GetMapping
+    @Operation(summary = "Get all restaurants")
     public ResponseEntity<Restaurant> getRestaurants() {
         /*Restaurant restaurant = new Restaurant();
         restaurant.setId("test");
@@ -40,6 +46,7 @@ public class RestaurantController {
     }
 
     @GetMapping("/nearby")
+    @Operation(summary = "Get nearby restaurants")
     public ResponseEntity<List<RestaurantDTO>> getNearbyRestaurants(
             @RequestParam("latitude") @NotNull @DecimalMin("-90.0") @DecimalMax("90.0") Double latitude,
             @RequestParam("longitude") @NotNull @DecimalMin("-180.0") @DecimalMax("180.0") Double longitude,
@@ -49,6 +56,7 @@ public class RestaurantController {
     }
 
     @GetMapping("/with-name")
+    @Operation(summary = "Get restaurants by name")
     public ResponseEntity<List<RestaurantDTO>> getRestaurantsByName(
             @RequestParam("name")
             @NotBlank(message = "Name cannot be blank")
@@ -60,24 +68,28 @@ public class RestaurantController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a restaurant")
     public ResponseEntity<RestaurantDTO> createRestaurant(@RequestBody CreateRestaurantRequest request) {
         RestaurantDTO restaurant = restaurantControllerContract.createRestaurant(request);
         return ResponseEntity.ok(restaurant);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a restaurant by id")
     public ResponseEntity<RestaurantDTO> getRestaurant(@NotNull @PathVariable String id) {
         RestaurantDTO restaurant = restaurantControllerContract.getRestaurant(id);
         return ResponseEntity.ok(restaurant);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a restaurant by id")
     public ResponseEntity<Void> deleteRestaurant(@NotNull @PathVariable String id) {
         restaurantControllerContract.deleteRestaurant(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a restaurant by id")
     public ResponseEntity<RestaurantDTO> updateRestaurant(@PathVariable String id, @RequestBody UpdateRestaurantRequest request) {
         RestaurantDTO restaurant = restaurantControllerContract.updateRestaurant(id, request);
         return ResponseEntity.ok(restaurant);
