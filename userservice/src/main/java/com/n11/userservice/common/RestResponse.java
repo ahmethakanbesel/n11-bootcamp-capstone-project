4ebc186066632fb17ed7e563cf1d2faa611e87e3
ahmethakanbesel <ahmethakan.besel@gmail.com>
2024-03-13 20:59:06 +0300

    user-service/fix: `RestResponse` unknown fields error

--- userservice/src/main/java/com/n11/userservice/common/RestResponse.java
+++ userservice/src/main/java/com/n11/userservice/common/RestResponse.java
@@ -1,5 +1,6 @@
 package com.n11.userservice.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RestResponse<T> {
     private T data;
     private LocalDateTime responseDate;
