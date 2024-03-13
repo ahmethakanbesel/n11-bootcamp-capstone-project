4b46e4d72f920fc58695b2285a831ae747c1f9b0
ahmethakanbesel <ahmethakan.besel@gmail.com>
2024-03-13 21:02:09 +0300

    user-service/refactor: disable custom logger

--- userservice/src/main/java/com/n11/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/n11/userservice/UserServiceApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
 public class UserServiceApplication {
+    //private static final Logger logger = LogManager.getLogger(UserServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
