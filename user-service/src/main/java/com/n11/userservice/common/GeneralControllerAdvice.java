34df87b8dff117ec87d3418fac36a35917ec56fb
ahmethakanbesel <ahmethakan.besel@gmail.com>
2024-03-14 14:49:39 +0300

    user-service/feat: append errors to kafka topics

--- user-service/src/main/java/com/n11/userservice/common/GeneralControllerAdvice.java
+++ user-service/src/main/java/com/n11/userservice/common/GeneralControllerAdvice.java
@@ -3,19 +3,34 @@ package com.n11.userservice.common;
 import com.n11.userservice.exceptions.BadRequestException;
 import com.n11.userservice.exceptions.ItemNotFoundException;
 import com.n11.userservice.exceptions.ResourceNotFoundException;
+import com.n11.userservice.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
+    private final KafkaProducerService kafkaProducerService;
+    private static final String ERRORS_500_TOPIC = "ERRORS_500";
+    private static final String ERRORS_404_TOPIC = "ERRORS_404";
+    private static final String ERRORS_400_TOPIC = "ERRORS_400";
+
     @ExceptionHandler
     public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
         String message = e.getMessage();
@@ -23,6 +38,26 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
 
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_500_TOPIC, message);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleAllExceptions(TransactionSystemException e, WebRequest request) {
+        var originalException = e.getOriginalException();
+        if (originalException == null) {
+            return handleAllExceptions(e, request);
+        }
+
+        String message = originalException.getCause().getMessage();
+        String description = request.getDescription(false);
+
+        var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
+        var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_500_TOPIC, message);
 
         return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -35,6 +70,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
 
+        kafkaProducerService.sendMessage(ERRORS_404_TOPIC, message);
+
         return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
     }
 
@@ -45,6 +82,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
 
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_500_TOPIC, message);
 
         return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -57,6 +96,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
 
+        kafkaProducerService.sendMessage(ERRORS_400_TOPIC, message);
+
         return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -67,6 +108,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
 
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_404_TOPIC, message);
 
         return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
     }
@@ -79,6 +122,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
 
+        kafkaProducerService.sendMessage(ERRORS_400_TOPIC, message);
+
         return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -89,6 +134,8 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
 
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_400_TOPIC, message);
 
         return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
@@ -101,6 +148,31 @@ public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
         var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), message, description);
         var restResponse = RestResponse.error(generalErrorMessages);
 
+        kafkaProducerService.sendMessage(ERRORS_400_TOPIC, message);
+
         return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers,
+                                                                  HttpStatusCode status, WebRequest request) {
+        List<Map<String, String>> errorList = e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError ->
+                {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(),
+                            fieldError.getDefaultMessage());
+                    return errorMap;
+                }).toList();
+
+        String description = request.getDescription(false);
+
+        var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), errorList.toString(), description);
+        var restResponse = RestResponse.error(generalErrorMessages);
+
+        kafkaProducerService.sendMessage(ERRORS_400_TOPIC, e.getMessage());
+
+        return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
