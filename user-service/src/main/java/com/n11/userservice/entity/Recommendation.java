dec095a7a03a7a75e1ccb697b227173e4914e1a4
ahmethakanbesel <ahmethakan.besel@gmail.com>
2024-03-14 21:02:23 +0300

    user-service/fix: cannot serialize

--- user-service/src/main/java/com/n11/userservice/entity/Recommendation.java
+++ user-service/src/main/java/com/n11/userservice/entity/Recommendation.java
@@ -4,9 +4,11 @@ import com.n11.userservice.dto.LocationDTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class Recommendation {
+public class Recommendation implements Serializable {
     String id;
     String name;
     String type;
